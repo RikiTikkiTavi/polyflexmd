219c06c40008e661dfe5272d681dfd4571f2c83d
Yahor Paromau <yahor.paromau@gmail.com>
2023-03-26 17:37:27 +0200

    initial1

--- experiments/FENE_beadspring/run_local.sh
+++ experiments/FENE_beadspring/run_local.sh
@@ -1,7 +1,7 @@
-#/bin/bash
+#!/bin/bash
 
 docker run \
- -v /home/egor/Projects/bachelor-thesis/experiments/FENE_beadspring:/home/lammps/experiments/FENE_beadspring \
- --entrypoint /usr/bin/lmp_mpi \
- --env OMP_NUM_THREADS=8 \
- lammps/lammps -in /home/lammps/experiments/FENE_beadspring/in.lammps
+  -v experiments/FENE_beadspring:/home/lammps/experiments/FENE_beadspring \
+  --entrypoint /usr/bin/lmp_serial \
+  --env OMP_NUM_THREADS=4 \
+  lammps/lammps -in /home/lammps/experiments/FENE_beadspring/lammps-input/in.lammps
