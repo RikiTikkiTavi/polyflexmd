871cc8b98b2db87d174925b674abf70cf0cb1a81
Yahor Paromau <yahor.paromau@gmail.com>
2023-04-19 16:46:23 +0200

    Try reproduce old results -> fene with only 20 chains;

--- experiments/1-FENE_beadspring/run_local.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker run \
-  -v experiments/FENE_beadspring:/home/lammps/experiments/FENE_beadspring \
-  --entrypoint /usr/bin/lmp_serial \
-  --env OMP_NUM_THREADS=4 \
-  lammps/lammps -in /home/lammps/experiments/FENE_beadspring/lammps-input/in.lammps
