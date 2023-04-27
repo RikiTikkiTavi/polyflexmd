cc52d6023c10ecc56539b5de5679f87aabe5a988
Yahor Paromau <yahor.paromau@gmail.com>
2023-04-27 19:15:01 +0200

    Report;

---
+++ scripts/notebook_job.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --job-name="polyflexmd-notebook"
+#SBATCH --output="/scratch/ws/0/s4610340-bt-eea1-md-workspace/slurm-%j.out"
+#SBATCH --error="/scratch/ws/0/s4610340-bt-eea1-md-workspace/slurm-%j.out"
+#SBATCH --account="p_mdpolymer"
+#SBATCH --time=8:00:00
+#SBATCH --partition=romeo
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem-per-cpu=5000
+
+module load modenv/hiera GCCcore/10.3.0 Python/3.9.5 GCC/10.3.0
+
+source /scratch/ws/0/s4610340-bt-eea1-md-workspace/polyflexmd/.venv/bin/activate
+
+export XDG_RUNTIME_DIR=""
+
+cd /scratch/ws/0/s4610340-bt-eea1-md-workspace/polyflexmd/notebooks && jupyter notebook --no-browser --port=8888
