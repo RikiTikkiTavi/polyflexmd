4330ba313605e677cc3f231b3beca62b7fd0014f
Yahor Paromau <yahor.paromau@gmail.com>
2023-05-16 14:47:36 +0200

    Add rouse theory;

---
+++ src/polyflexmd/data_analysis/theory/rouse.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+def rouse_R_sq_avg(t: np.ndarray, N_b: int, l_b: float) -> np.ndarray:
+    return np.repeat(N_b * l_b ** 2, t.shape)
+
+
+def rouse_R_autocorr(t: np.ndarray, p_max: int, N_b: int, l_b: float, tau_R: float) -> np.ndarray:
+    result = 0
+    for p in range(1, p_max):
+        if p % 2 == 0:
+            continue
+        result += 1 / p ** 2 * np.exp(-t * p ** 2 / tau_R)
+    return 8 * N_b * l_b ** 2 / np.pi ** 2 * result
+
+
+def rouse_g_4(t: np.ndarray, tau_R: float, p_max: int, N_b: int, l_b: float) -> np.ndarray:
+    s = 0
+    for p in range(1, p_max + 1):
+        if p % 2 == 0:
+            continue
+        s += 1 / p ** 2 * np.exp(-t * p ** 2 / tau_R)
+    return 2 * N_b * l_b ** 2 * (1 - 8 / np.pi ** 2 * s)
